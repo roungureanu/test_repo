df4b122e3a979604324c69fb2a56d29e392d6b60
Robert-Mihail UNGUREANU <roungureanu@bitdefender.com>
2019-05-06 21:08:41 +0300

    Update

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import time
 class Worker:
 	def run(self):
 		while True:
+			try:
+				with open(r'D:\temp\whatever.txt', 'a') as handle:
+					handle.write('ok\n')
+			except Exception as exc:
+				pass
 			print('ok')
 			time.sleep(5)
 
