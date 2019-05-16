2acac559e699514ed4e35089470b92e1c51eb441
Robert-Mihail UNGUREANU <roungureanu@bitdefender.com>
2019-05-16 19:50:47 +0300

    Whatevs

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import sys
 import time
 
 class Worker:
 	def run(self):
 		while True:
+                        with open(r'D:\temp\args.txt', 'a') as handle:
+				handle.write(str(sys.args))
 			try:
 				with open(r'D:\temp\whatever.txt', 'a') as handle:
 					handle.write('ok\n')
